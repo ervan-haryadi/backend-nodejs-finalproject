7c378324a32fda0aa2d9103c7217c2367b414fe6
Ervan Haryadi <ervan.a.haryadi@gmail.com>
2024-05-25 19:13:11 +0700

    add auth functions

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const SECRET_KEY = 'your_secret_key';
 
 mongoose.set('strictQuery', false);
 
-const uri =  "mongodb://root:<replace password>@localhost:27017";
-mongoose.connect(uri,{'dbName':'SocialDB'});
+const uri = "mongodb://root:<replace password>@localhost:27017";
+mongoose.connect(uri, { 'dbName': 'SocialDB' });
 
 const User = mongoose.model('User', { username: String, email: String, password: String });
 const Post = mongoose.model('Post', { userId: mongoose.Schema.Types.ObjectId, text: String });
@@ -22,9 +22,30 @@ app.use(session({ secret: SECRET_KEY, resave: false, saveUninitialized: true, co
 
 
 // Insert your authenticateJWT Function code here.
+function authenticateJWT(req, res, next) {
+    const token = req.session.token;
+    if (!token) return res.status(401).json({ message: 'Unauthorized' });
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid token' });
+    };
+};
 
 // Insert your requireAuth Function code here.
-
+function requireAuth(req, res, next) {
+    const token = req.session.token;
+    if (!token) return res.redirect('/login');
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.redirect('/login');
+    };
+};
 // Insert your routing HTML code here.
 
 // Insert your user registration code here.
