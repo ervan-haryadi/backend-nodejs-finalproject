03f887a42e8d53a1056aa3acf5ddf221d70b3707
Ervan Haryadi <ervan.a.haryadi@gmail.com>
2024-05-25 19:15:17 +0700

    add routing

--- app.js
+++ app.js
@@ -47,6 +47,25 @@ function requireAuth(req, res, next) {
     };
 };
 // Insert your routing HTML code here.
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+);
+
+app.get('/register', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public', 'register.html'))
+);
+
+app.get('/login', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public', 'login.html'))
+);
+
+app.get('/post', requireAuth, (req, res) =>
+    res.sendFile(path.join(__dirname, 'public', 'post.html'))
+);
+
+app.get('/index', requireAuth, (req, res) =>
+    res.sendFile(path.join(__dirname, 'public', 'index.html'), { username: req.user.username })
+);
 
 // Insert your user registration code here.
 
